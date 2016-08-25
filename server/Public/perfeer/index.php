f31d70fa0d437d2a96bf94d105f80ad57bda650b
Jianhua <jianhua.he@cn.ta-mp.com>
2016-08-25 19:05:04 +0800

    init perfeer server project

--- server/Public/perfeer/index.php
+++ server/Public/perfeer/index.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * $APP_NAME 统一入口
+ * Perfeer 统一入口
  */
 
-require_once dirname(__FILE__) . '/init.php';
+require_once dirname(__FILE__) . '/../init.php';
 
 //装载你的接口
 DI()->loader->addDirs('Perfeer');
